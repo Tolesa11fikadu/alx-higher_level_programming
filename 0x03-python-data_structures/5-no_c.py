6a469f7df638c20d4eb44ea23829cc4d6af874be
Tolesa11fikadu <tolee4530@gmail.com>
2022-12-03 23:31:36 -0800

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""function that removes all characters c and C from a string"""
+
+def no_c(my_string):
+    new_string = ''
+    for char in my_string:
+        if char == 'c' or char == 'C':
+            continue
+        new_string += char
+    return new_string 
