e865951d86a2534a6f4444a4122df63b0a100e70
Tolesa11fikadu <tolee4530@gmail.com>
2022-12-13 09:23:51 -0800

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 
-""" a function that computes the
-square value of all integers of a matrix"""
-
-
 def square_matrix_simple(matrix=[]):
-    new_mat = [x[:] for x in matrix]
-    for i in range(len(new_mat)):
-        for j in range(len(new_mat[i])):
-            new_mat[i][j] *= new_mat[i][j]
-            return new_mat
+    """Compute the square value of all integers of a matrix."""
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
